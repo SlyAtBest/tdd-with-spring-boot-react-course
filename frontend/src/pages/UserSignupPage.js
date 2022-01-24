b85efdbf8352018a9c973c22da2857e2d4895a43
mat <slyatbest@gmail.com>
2022-01-24 17:37:13 +0000

    Completed chapter 16.
    Added form data to UserSignupPage and
    accompanying tests. Refactored test code to use
    modern conventions.

--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -4,6 +4,21 @@ export const UserSignupPage = () => {
     return (
     <div>
         <h1>Sign Up</h1>
+        <div>
+            <input placeholder="Your display name" />
+        </div>
+        <div>
+            <input placeholder="Your username" />
+        </div>
+        <div>
+            <input placeholder="Your password" type="password" />
+        </div>
+        <div>
+            <input placeholder="Repeat your password" type="password" />
+        </div>
+        <div>
+            <button>Sign Up</button>
+        </div>
     </div>
     );
 }
