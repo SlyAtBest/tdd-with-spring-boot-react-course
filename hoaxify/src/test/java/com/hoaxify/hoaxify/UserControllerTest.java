de33ff90db62cbcba5a2d6217408ff0c5e18d096
mat <slyatbest@gmail.com>
2022-01-24 13:05:30 +0000

    Completed chapter 14.
    Re-enabled the dependency on Spring Security to
    access the password encoder.
    
    Updated the main application to exclude the
    SecurityAutoConfiguration class to prevent it
    locking down all endpoints at this stage.
    
    Updated application config to allow access to the
    h2 database via /h2-console and added a setting
    to prevent it generating a unique name each time.
    
    Updated UserService to hash the user password
    using the BCryptPasswordEncoder.
    
    Added unit test to check password hashing takes
    place.

--- hoaxify/src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
+++ hoaxify/src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
@@ -14,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -51,9 +53,17 @@ public class UserControllerTest {
     @Test
     public void postUser_whenUserIsValid_receiveSuccessMessage() {
         User user = createValidUser();
-
         ResponseEntity<GenericResponse> response = testRestTemplate.postForEntity(API_1_0_USERS, user, GenericResponse.class);
         assertThat(response.getBody().getMessage()).isNotNull();
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_passwordIsHashedInDatabase() {
+        User user = createValidUser();
+        ResponseEntity<GenericResponse> response = testRestTemplate.postForEntity(API_1_0_USERS, user, GenericResponse.class);
+        List<User> users = userRepository.findAll();
+        User inDB = users.get(0);
+        assertThat(inDB.getPassword()).isNotEqualTo(user.getPassword());
     }
 
     private User createValidUser() {
