b7122393d2f1df6f3d82d3793d3b384f5baa8754
mat <slyatbest@gmail.com>
2022-01-24 12:36:04 +0000

    Completed chapter 13.
    Added GenericResponse object to allow custom
    responses to the UserController.
    
    Refactored the UserController createUser method
    to return this new GenericResponse type with
    a sensible message.
    
    Added unit test to cover this new behaviour.

--- hoaxify/src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
+++ hoaxify/src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.hoaxify.hoaxify;
 
+import com.hoaxify.hoaxify.shared.GenericResponse;
 import com.hoaxify.hoaxify.user.User;
 import com.hoaxify.hoaxify.user.UserRepository;
 import org.junit.Before;
@@ -40,18 +41,26 @@ public class UserControllerTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
+    @Test
+    public void postUser_whenUserIsValid_userSaveToDatabase() {
+        User user = createValidUser();
+        testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
+        assertThat(userRepository.count()).isEqualTo(1);
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_receiveSuccessMessage() {
+        User user = createValidUser();
+
+        ResponseEntity<GenericResponse> response = testRestTemplate.postForEntity(API_1_0_USERS, user, GenericResponse.class);
+        assertThat(response.getBody().getMessage()).isNotNull();
+    }
+
     private User createValidUser() {
         User user = new User();
         user.setUsername("test-user");
         user.setDisplayName("test-display");
         user.setPassword("P4ssword");
         return user;
-    }
-
-    @Test
-    public void postUser_whenUserIsValid_userSaveToDatabase() {
-        User user = createValidUser();
-        testRestTemplate.postForEntity(API_1_0_USERS, user, Object.class);
-        assertThat(userRepository.count()).isEqualTo(1);
     }
 }
